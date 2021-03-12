9f198845d4e8e1b6ca38dec445235936f0b910fb
adhiatmaka purna <adiatmaka@gmail.com>
2021-03-12 15:22:00 +0700

    Good

--- I0320001_Soal1_Tugas2.py
+++ I0320001_Soal1_Tugas2.py
@@ -1,26 +1,31 @@
+#Persegi Panjang
 print("==== Menghitung Luas dari Persegi Panjang ====")
-panjang = float(input("Panjang dari persegi panjang:"))
-lebar = float(input("Lebar dari persegi panjang:"))
-luas = (panjang*lebar)
-print("Luas dari persegi panjang sama dengan",luas,"satuan")
+Panjang = float(input("Panjang dari persegi panjang:"))
+Lebar = float(input("Lebar dari persegi panjang:"))
+Luas = (Panjang*Lebar)
+print("Luas dari persegi panjang sama dengan",Luas,"satuan")
 
+#Lingkaran
 print("==== Menghitung Luas dari Lingkaran ====")
 phi = 3.14
-jari_jari = float(input("Jari-jari lingkaran :"))
-luas = jari_jari*jari_jari*phi
-print("Luas dari lingkaran sama dengan",luas,"satuan")
+Jari_jari = float(input("Jari-jari lingkaran :"))
+Luas = Jari_jari*Jari_jari*phi
+print("Luas dari lingkaran sama dengan",Luas,"satuan")
 
+#Kubus
 print("===== Menghitung dari Luas Kubus =====")
-sisi = float(input("Sisi Kubus :"))
-luas_permukaan = 6*sisi*sisi
-print("Luas dari permukaan kubus sama dengan",luas_permukaan,"satuan")
+Sisi = float(input("Sisi Kubus :"))
+Luas_permukaan = 6*Sisi*Sisi
+print("Luas dari permukaan kubus sama dengan",Luas_permukaan,"satuan")
 
+#Konversi Suhu Celcius ke Fahrenheit
 print("===== Menghitung Konversi Suhu dari Celcius ke Fahrenheit")
-celcius = float(input("Suhu dalam celcius :"))
-konversi = (((9/5)*celcius)+32)
-print("Konversi suhu dari celcius ke fahrenheit sama dengan",konversi,"derajat")
+Celcius = float(input("Suhu dalam celcius :"))
+Konversi_suhu = (((9/5)*Celcius)+32)
+print("Konversi suhu dari celcius ke fahrenheit sama dengan",Konversi_suhu,"°F")
 
+#Konversi Suhu Reamur ke Kelvin
 print("Menghitung konversi suhu dari Reamur ke Kelvin")
-reamur = float(input("Suhu dalam reamur :"))
-konversi = ((5/4)*reamur)+273
-print("Konversi suhu dari reamur ke kelvin sama dengan",konversi,"derajat")
+Reamur = float(input("Suhu dalam reamur :"))
+Konversi_suhu = ((5/4)*Reamur)+273
+print("Konversi suhu dari reamur ke kelvin sama dengan",Konversi_suhu,"K")
